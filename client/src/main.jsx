0ced94d41c3952b13a27557f174df4bdbe1db56f
obsell <58229379+dongq845@users.noreply.github.com>
2025-07-29 15:39:35 +0900

    Create Routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,24 @@
 // ba-worldcup/src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import NotFoundPage from "./NotFoundPage.jsx";
 
 // This line tells React to take control of the 'root' div.
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Route path="/" element={<App />} />
-    <Route path="*" element={<NotFoundPage />} />
+    <BrowserRouter>
+      <Routes>
+        {/* Route 1: The Homepage */}
+        {/* If the URL path is exactly "/", render the main App component. */}
+        <Route path="/" element={<App />} />
+
+        {/* Route 2: The "Catch-All" or "Not Found" Route */}
+        {/* The path="*" is a wildcard that matches ANY other URL. */}
+        {/* If no other route matches, this one will be rendered. */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
