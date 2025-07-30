6dc279b1f99b03f285d932b69b2940de7a52b465
obsell <58229379+dongq845@users.noreply.github.com>
2025-07-30 14:31:39 +0900

    Create ScrollToTop.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import NotFoundPage from "./NotFoundPage.jsx";
 import AboutPage from "./AboutPage.jsx";
+import ScrollToTop from "./ScrollToTop.jsx";
 
 // This line tells React to take control of the 'root' div.
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         {/* Route 1: The Homepage */}
         <Route path="/" element={<App />} />
